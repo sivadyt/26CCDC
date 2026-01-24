fbc148d602106d0c2229ed8e5bad2b4607eb2c75
Rugnarokk <55717301+Rugnarokk@users.noreply.github.com>
2026-01-24 14:58:32 -0600

    Update IPBlock.ps1

--- IPBlock.ps1
+++ IPBlock.ps1
@@ -1,7 +1,7 @@
-$ipblock = ReadHost "Enter IP address to block"
+$ipblock = Read-Host "Enter IP address to block"
 
-WriteHost "Blocking"
+Write-Host "Blocking"
 New-NetFirewallRule -DisplayName "CCDC-Block $ipblock" -Action Block -RemoteAddress $ipblock
 New-NetFirewallRule -DisplayName "CCDC-Block $ipblock" -Action Block -RemoteAddress $ipblock -Direction Outbound
 
-ReadHost "Done. Press enter to exit..."
+Read-Host "Done. Press enter to exit..."
